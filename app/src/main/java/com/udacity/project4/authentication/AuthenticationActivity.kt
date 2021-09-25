bc2de30e76b2f9b39a4b47f5f0a8e5054410374a
ira <ira@ira>
2021-09-24 21:57:20 -0500

    add login/logout functionality

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -4,13 +4,16 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -24,33 +27,35 @@ class AuthenticationActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityAuthenticationBinding
+    private val viewModel by viewModels<LoginViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_authentication)
 
-        //set up onclicklistener on the login button to start the flow
-        binding.loginButton.setOnClickListener {
-            launchSignInFlow()
-        }
-
-
-
-
-
-
-
-
-
-//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
-
-//          TODO: If the user was authenticated, send him to RemindersActivity
+        viewModel.authenticationState.observe(this,
+            Observer { authenticationState ->
+                when (authenticationState) {
+                    LoginViewModel.AuthenticationState.AUTHENTICATED -> {
+                        //If the user was authenticated, send him to RemindersActivity
+                        startActivity(Intent(this, RemindersActivity::class.java))
+                        finish()
+                    }
+                    else -> {
+                        //set up onclicklistener on the login button to start the flow
+                        binding.loginButton.setOnClickListener {
+                            launchSignInFlow()
+                        }
+                    }
+                }
+            })
 
 //          TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
 
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         // TODO Listen to the result of the sign in process by filter for when
@@ -60,7 +65,10 @@ class AuthenticationActivity : AppCompatActivity() {
             val response = IdpResponse.fromResultIntent(data)
             if (resultCode == Activity.RESULT_OK) {
                 // User successfully signed in
-                Log.i(TAG, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+                Log.i(
+                    TAG,
+                    "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!"
+                )
             } else {
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise check
@@ -75,17 +83,17 @@ class AuthenticationActivity : AppCompatActivity() {
         // If users choose to register with their email,
         // they will need to create a password as well.
         val providers = arrayListOf(
-                AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
         )
         // Create and launch sign-in intent.
         // We listen to the response of this activity with the
         // SIGN_IN_REQUEST_CODE
         startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .build(),
-                SIGN_IN_REQUEST_CODE
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            SIGN_IN_REQUEST_CODE
         )
     }
 
