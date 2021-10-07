8e656a84c541dc627420ad8ea39a17f9de39eddb
ira <ira@ira>
2021-10-07 11:52:14 -0500

    add fake data implementation

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,40 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
 
-    //TODO: provide testing to the RemindersListViewModel and its live data objects
+    // Subject under test
+    private lateinit var remindersListViewModel: RemindersListViewModel
 
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setupViewModel() {
+        remindersListViewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext())
+    }
+
+    @Test
+    fun invalidateShowNoData_setsNewTaskEvent() {
+
+        // Given a fresh TasksViewModel
+
+
+        // When adding a new task
+
+
+        // Then the new task event is triggered
+
+    }
 }
