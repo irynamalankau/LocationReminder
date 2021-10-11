5c1b34bad01c4bc58fe1f0ebd4734c869687a503
ira <ira@ira>
2021-10-11 16:54:09 -0500

    add dao tests

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
@@ -25,6 +25,63 @@ import org.junit.Test
 @SmallTest
 class RemindersDaoTest {
 
-//    TODO: Add testing implementation to the RemindersDao.kt
+    //Add testing implementation to the RemindersDao.kt
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var database: RemindersDatabase
+    private lateinit var dao: RemindersDao
+
+    private val reminder = ReminderDTO(
+        title = "title",
+        description = "description",
+        location = "location",
+        latitude = 42.08780700912105,
+        longitude = -87.78266481484444
+    )
+
+    @Before
+    fun setup() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java
+        ).build()
+        dao = database.reminderDao()
+    }
+
+    @After
+    fun closeDb() = database.close()
+
+
+
+    @Test
+    fun insertReminderAndGetById() = runBlockingTest {
+        // GIVEN - Insert a reminder.
+        dao.saveReminder(reminder)
+
+        // WHEN - Get the reminder by id from the database.
+        val loaded = dao.getReminderById(reminder.id)
+
+        // THEN - The loaded data contains the expected values.
+        assertThat<ReminderDTO>(loaded as ReminderDTO, notNullValue())
+        assertThat(loaded.id, `is`(reminder.id))
+        assertThat(loaded.title, `is`(reminder.title))
+        assertThat(loaded.description, `is`(reminder.description))
+        assertThat(loaded.location, `is`(reminder.location))
+        assertThat(loaded.latitude, `is`(reminder.latitude))
+        assertThat(loaded.longitude, `is`(reminder.longitude))
+    }
+
+    @Test
+    fun deleteAllReminders() = runBlockingTest {
+        // GIVEN - Insert a reminder.
+        dao.saveReminder(reminder)
+
+        // WHEN - All reminders are deleted
+        dao.deleteAllReminders()
+
+        // THEN - The db entity is empty
+        val dbSize = dao.getReminders().size
+        assertThat(dbSize, `is`(0))
+    }
 }
