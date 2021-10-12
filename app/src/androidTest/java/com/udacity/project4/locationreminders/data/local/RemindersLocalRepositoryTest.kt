8c2d984579d064e7e36c0b1d2ff890168e8c3c92
ira <ira@ira>
2021-10-12 11:08:57 -0500

    add reminderslist view model tests

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
@@ -11,7 +11,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Before
@@ -76,7 +75,7 @@ class RemindersLocalRepositoryTest {
     }
 
     @Test
-    fun deleteAllReminders_getReminderById() = runBlocking {
+    fun deleteAllReminders_getReminderById_getError() = runBlocking {
         // GIVEN - All reminders are deleted from the database
         remindersRepository.saveReminder(reminder)
         remindersRepository.deleteAllReminders()
--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,14 +1,25 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.os.Build
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
+import org.robolectric.annotation.Config
 
+@Config(sdk = [Build.VERSION_CODES.O])
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
@@ -16,25 +27,74 @@ class RemindersListViewModelTest {
     // Subject under test
     private lateinit var remindersListViewModel: RemindersListViewModel
 
+    // Use a fake data source to be injected into the view model.
+    private lateinit var fakeDataSource: FakeDataSource
+
+    private val remindersList = mutableListOf(
+        ReminderDTO(
+            title = "title",
+            description = "description",
+            location = "location",
+            latitude = 42.08780700912105,
+            longitude = -87.78266481484444
+        ), ReminderDTO(
+            title = "title",
+            description = "description",
+            location = "location",
+            latitude = 42.08780700912105,
+            longitude = -87.78266481484444
+        )
+    )
+
     // Executes each task synchronously using Architecture Components.
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
     @Before
     fun setupViewModel() {
-        remindersListViewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext())
+        stopKoin()
+        fakeDataSource = FakeDataSource(remindersList)
+        remindersListViewModel =
+            RemindersListViewModel(ApplicationProvider.getApplicationContext(), fakeDataSource)
     }
 
     @Test
-    fun invalidateShowNoData_setsNewTaskEvent() {
+    fun loadReminders_loading() = runBlockingTest {
+        // Pause dispatcher so you can verify initial values.
+        mainCoroutineRule.pauseDispatcher()
 
-        // Given a fresh TasksViewModel
+        // Load the reminder in the view model.
+        remindersListViewModel.loadReminders()
 
+        // Then assert that the progress indicator is shown.
+        assertThat(remindersListViewModel.showLoading.getOrAwaitValue(), `is`(true))
 
-        // When adding a new task
+        // Execute pending coroutines actions.
+        mainCoroutineRule.resumeDispatcher()
 
+        // Then assert that the progress indicator is hidden.
+        assertThat(remindersListViewModel.showLoading.getOrAwaitValue(), `is`(false))
+        assertThat(remindersListViewModel.showNoData.getOrAwaitValue(), `is`(false))
+    }
 
-        // Then the new task event is triggered
+    @Test
+    fun loadReminders_dataSourceReturnsError_showSnackBarErrorMessage() = runBlockingTest {
+        fakeDataSource.setReturnError(true)
+        remindersListViewModel.loadReminders()
 
+        assertThat(
+            remindersListViewModel.showSnackBar.getOrAwaitValue(),
+            `is`("Reminders not found")
+        )
+    }
+
+    @Test
+    fun invalidateShowNoData_emptyDataSource_showNoDataTrue() = runBlockingTest{
+        fakeDataSource.deleteAllReminders()
+        remindersListViewModel.loadReminders()
+        assertThat(remindersListViewModel.showNoData.getOrAwaitValue(), `is`(true))
     }
 }
