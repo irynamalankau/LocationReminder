2e1550cd6e4393393c38bb2b8a674e7f51f9ea96
ira <ira@ira>
2021-10-12 19:37:06 -0500

    update tests

--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,16 +1,104 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.getOrAwaitValue
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.MatcherAssert.assertThat
+
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
 
+    private lateinit var saveReminderViewModel: SaveReminderViewModel
+    private lateinit var fakeDataSource: FakeDataSource
 
-    //TODO: provide testing to the SaveReminderView and its live data objects
+    // Executes each task synchronously using Architecture Components.
+    // This rule ensures that the test results happen synchronously and in a repeatable order.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    @Before
+    fun setupViewModel() {
+        stopKoin()
+        fakeDataSource = FakeDataSource()
+        saveReminderViewModel = SaveReminderViewModel(
+                ApplicationProvider.getApplicationContext(),
+                fakeDataSource
+        )
+    }
+
+    private val reminder = ReminderDataItem(
+            title = "title",
+            description = "description",
+            location = "location",
+            latitude = 42.08780700912105,
+            longitude = -87.78266481484444
+    )
+
+    @Test
+    fun check_loading() = runBlockingTest{
+
+        // The loading animation appeared.
+        mainCoroutineRule.pauseDispatcher()
+
+        saveReminderViewModel.validateAndSaveReminder(reminder)
+        assertThat(saveReminderViewModel.showLoading.getOrAwaitValue(), `is`(true))
+
+        // The loading animation disappeared.
+        mainCoroutineRule.resumeDispatcher()
+        assertThat(saveReminderViewModel.showLoading.getOrAwaitValue(), `is`(false))
+    }
+
+    @Test
+    fun saveReminder_reminderSaved() = runBlockingTest {
+        saveReminderViewModel.saveReminder(reminder)
+        assertThat(saveReminderViewModel.showToast.getOrAwaitValue(), `is`("Reminder Saved!"))
+    }
+
+    @Test
+    fun saveReminder_noTitle() = runBlockingTest {
+        val reminder = ReminderDataItem(
+                title = "",
+                description = "description",
+                location = "location",
+                latitude = 42.08780700912105,
+                longitude = -87.78266481484444)
+
+        saveReminderViewModel.validateAndSaveReminder(reminder)
+        assertThat(saveReminderViewModel.showSnackBarInt.getOrAwaitValue(), notNullValue())
+    }
+
+    @Test
+    fun saveReminder_noLocation() = runBlockingTest {
+        val reminder = ReminderDataItem(
+                title = "title",
+                description = "description",
+                location = "",
+                latitude = 42.08780700912105,
+                longitude = -87.78266481484444)
+
+        saveReminderViewModel.validateAndSaveReminder(reminder)
+        assertThat(saveReminderViewModel.showSnackBarInt.getOrAwaitValue(), notNullValue())
+
+    }
+
 
 
 }
